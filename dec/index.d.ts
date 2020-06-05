afc7bff9e8b48ec639e30a1c5e755eb0f388c045
sergeichikan <[T#jxg62jx.KxS1%g5+qjx=9]>
2020-06-05 13:43:36 +0300

    new

--- dec/index.d.ts
+++ dec/index.d.ts
@@ -1,4 +1,6 @@
+import { form } from "./form";
+import { transport } from "./transport";
 import { log } from "./log";
 import { Logger, Form, Transport } from "./logger";
 import { trace, debug, error, fatal, info, warn } from "./logs";
-export { log, Logger, Form, Transport, trace, debug, error, fatal, info, warn, };
+export { form, transport, log, Logger, Form, Transport, trace, debug, error, fatal, info, warn, };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { form } from "./form";
+import { transport } from "./transport";
 import { log } from "./log";
 import { Logger, Form, Transport } from "./logger";
 import { trace, debug, error, fatal, info, warn } from "./logs";
 
 export {
+    form,
+    transport,
     log,
     Logger,
     Form,
