9c709474a331add5f09486634551291fd821269b
sergeichikan <[T#jxg62jx.KxS1%g5+qjx=9]>
2020-06-22 19:05:08 +0300

    v2.1.1 add child

--- src/logger.ts
+++ src/logger.ts
@@ -6,41 +6,49 @@ import { Form } from "./form";
 import { Transport } from "./transport";
 
 export class Logger extends EventEmitter {
+
+    public readonly form: Form;
+    public readonly transport: Transport;
+
     public constructor(
         form: Form = defaultForm,
         transport: Transport = defaultTransport,
     ) {
         super();
+        this.form = form;
+        this.transport = transport;
         this.on("transport", transport);
-        this.on("trace", (msg: unknown) => this.emit("transport", form(1, msg)));
-        this.on("debug", (msg: unknown) => this.emit("transport", form(2, msg)));
-        this.on("info", (msg: unknown) => this.emit("transport", form(3, msg)));
-        this.on("warn", (msg: unknown) => this.emit("transport", form(4, msg)));
-        this.on("error", (msg: unknown) => this.emit("transport", form(5, msg)));
-        this.on("fatal", (msg: unknown) => this.emit("transport", form(6, msg)));
+    }
+
+    public child(childForm: Form) {
+        const { form } = this;
+        return new Logger((lvl, msg) => ({
+            ...form(lvl, msg),
+            ...childForm(lvl, msg),
+        }), (msg) => this.emit("transport", msg))
     }
 
     public trace(msg: unknown) {
-        this.emit("trace", msg);
+        this.emit("transport", this.form(1, msg));
     }
 
     public debug(msg: unknown) {
-        this.emit("debug", msg);
+        this.emit("transport", this.form(2, msg));
     }
 
     public info(msg: unknown) {
-        this.emit("info", msg);
+        this.emit("transport", this.form(3, msg));
     }
 
     public warn(msg: unknown) {
-        this.emit("warn", msg);
+        this.emit("transport", this.form(4, msg));
     }
 
     public error(msg: unknown) {
-        this.emit("error", msg);
+        this.emit("transport", this.form(5, msg));
     }
 
     public fatal(msg: unknown) {
-        this.emit("fatal", msg);
+        this.emit("transport", this.form(6, msg));
     }
 }
